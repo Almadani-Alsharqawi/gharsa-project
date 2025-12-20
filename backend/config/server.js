0d6b992d480b31931b859a049777c65db5ffd430
Almadani-Alsharqawi <almadani.alsharqawi@gmail.com>
2025-12-20 08:03:30 +0200

    Update server.js

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,15 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  // الرابط العام للمشروع
   url: env('STRAPI_URL', 'https://strapi.gharsa.ly'),
-  proxy: true, // تفعيل البروكسي بشكل إجباري
-  admin: {
-    // تحديد رابط الأدمن بوضوح كما في الحل الذي وجدته
-    url: env('STRAPI_ADMIN_URL', '/admin'),
-    auth: {
-      secret: env('ADMIN_JWT_SECRET'),
-    },
+  // الصيغة الجديدة الموصى بها لـ Strapi v5
+  proxy: {
+    koa: true,
   },
   app: {
     keys: env.array('APP_KEYS'),
